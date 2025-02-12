8c2b45a008dd52e4512cc05f8f09003b84a7b91a
jimanloi <jimanloi@hotmail.com>
2025-02-12 13:48:44 +0100

    isolate exercises

--- lesson-plans/devtools-debugger/1-isolate/exercises/1-conditionals/1-greeting.js
+++ lesson-plans/devtools-debugger/1-isolate/exercises/1-conditionals/1-greeting.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 let input = prompt('enter your name:');
 
 let greeting = '';
@@ -11,7 +12,25 @@ if (input === null) {
 
 alert(greeting);
 
+*/
+
 /*  ===== Challenges =====
   - Ask the user for their first and last names.
   - Greet them with their full name.
 */
+
+const firstName = prompt('Enter your first name:');
+const lastName = prompt('Enter your last name:');
+
+let greeting = '';
+if (firstName === '' && lastName === '') {
+  greeting = 'Hello stranger!';
+} else if (firstName === '' && lastName !== '') {
+  greeting = `Hello ${lastName}!`;
+} else if (firstName !== '' && lastName === '') {
+  greeting = `Hello ${firstName}!`;
+} else {
+  greeting = `Hello ${firstName} ${lastName}!`;
+}
+
+alert(greeting);
--- lesson-plans/devtools-debugger/1-isolate/exercises/2-while-loops/2-five-characters.js
+++ lesson-plans/devtools-debugger/1-isolate/exercises/2-while-loops/2-five-characters.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let input = null;
 while (input === null || input.length !== 5) {
   input = prompt('enter something with 5 characters:');
@@ -8,8 +8,33 @@ while (input === null || input.length !== 5) {
 let message = 'thank you for "' + input + '"!';
 
 alert(message);
+*/
 
 /*  ===== Challenges =====
   - Change the condition to 10 characters.
   - Make the user write "everything" in 2 steps.
 */
+
+// ===== Challenge 1 =====
+
+/*
+let input = null;
+while (input === null || input.length !== 10) {
+  input = prompt('Enter 10 characters.')
+}
+
+let message = 'Thank you!'
+
+alert(message)
+*/
+
+// ===== Challenge 2 =====
+
+let input = null;
+while (input === null || input !== 'everything') {
+  input = prompt("Type 'everything'");
+}
+
+let message = 'Thank you!';
+
+alert(message);
